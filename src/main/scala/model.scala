835bec97531b2d875857ef114c64f66ae89fd155
Xyouz <marius.dufraisse@gmail.com>
2018-02-16 18:44:02 +0100

    The main function does some tests

--- src/main/scala/model.scala
+++ src/main/scala/model.scala
@@ -62,7 +62,11 @@ class Train(val leaving: Town, val destination: Town, val speed : Double){
 object Program
 {
   def main(args: Array[String])= {
-       	   val y = new Town("Bordeaux", 132, List(("bananes", 12), ("mures", 32)), new Point(4,5) )
-           y.position().print()
+    val bordeaux = new Town("Bordeaux", 132, List(("bananes", 12), ("mures", 32)), new Point(4,5) )
+    val agen = new Town("Agen", 132, List(("bananes", 12), ("mures", 32)), new Point(0,5) )
+    val t = new Train(bordeaux, agen,1)
+    val r = new Road(bordeaux, agen,Array(bordeaux.position(),agen.position()))
+    val p = r.position(2.5)
+    p._1.print()
 	 }
 }
