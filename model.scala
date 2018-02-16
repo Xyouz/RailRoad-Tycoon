7976d5cf90599bf30d3374de7931f4d55e6ff4ed
Xyouz <marius.dufraisse@gmail.com>
2018-02-16 18:03:36 +0100

    Ajout des classes Goods, Town et Train

--- model.scala
+++ model.scala
@@ -39,3 +39,24 @@ class Road(val begin : Town,val end : Town,val route : Array[Point]){
     }
   }
 }
+
+
+class Goods (val namegoods : String, val pricetag : Float ){}
+
+
+class Town(val name: String,val pop : Int, val listofgoods :List[(String, Int)]) {}
+
+
+class Train(val leaving: Town, val destination: Town, val speed : Float){
+    var passengers = leaving.pop
+    var goodies = leaving.listofgoods
+    var loaded: Int = 0
+    def loading(): Unit = {loaded = (0.1*passengers).toInt}
+}
+
+
+def main()= {
+       	   val y = new Town("Bordeaux", 132, List(("bananes", 12), ("mures", 32)) )
+	   println(y)
+	   }
+}
