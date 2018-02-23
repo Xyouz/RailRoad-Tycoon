7257b41ec73ac36c60ab225fd2ecf674ed1ab461
Xyouz <marius.dufraisse@gmail.com>
2018-02-23 17:24:24 +0100

    Modification de l'arborescence du projet pour permettre l'utilisation de packages

--- src/main/scala/model/Model.scala
+++ src/main/scala/model/Model.scala
@@ -64,17 +64,7 @@ class Train(val speed : Double){
     def loading(): Unit = {loaded = (0.1*passengers).toInt}*/
 }
 
-object Program
-{
-  def main(args: Array[String])= {
-    val bordeaux = new Town("Bordeaux", 132, List(("bananes", 12), ("mures", 32)), new Point(4,5) )
-    val agen = new Town("Agen", 132, List(("bananes", 12), ("mures", 32)), new Point(0,5) )
-    val t = new Train(bordeaux, agen,1)
-    val r = new Road(bordeaux, agen,Array(bordeaux.position(),agen.position()))
-    val p = r.position(2.5)
-    p._1.print()
-	 }
-}
+
 
 
 class Game
