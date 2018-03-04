96f06fa397615c861317b223fa07ac394419d1e8
Xyouz <marius.dufraisse@gmail.com>
2018-03-04 15:26:19 +0100

    Affichage de la population paufiné

--- src/main/scala/gui/GUI.scala
+++ src/main/scala/gui/GUI.scala
@@ -2,14 +2,17 @@ package gui
 
 import model._
 import scalafx.Includes._
-import scalafx.application.JFXApp
+import scalafx.application.{JFXApp, Platform}
 import scalafx.scene.Scene
-import scalafx.scene.layout.{HBox,VBox,BorderPane}
+import scalafx.scene.layout.{HBox,VBox,BorderPane,Priority,GridPane}
 import scalafx.scene.paint.Color._
 import scalafx.scene.shape.{Circle,Rectangle,Line}
 import scalafx.beans.property.DoubleProperty
 import scalafx.animation.AnimationTimer
-import scalafx.scene.control.{Button, Label}
+import scalafx.scene.control.Alert.AlertType
+import scalafx.scene.control._
+import scalafx.scene.control.ButtonBar.ButtonData
+import scalafx.geometry.{Insets, Pos}
 
 class MainGame(val game: Game) extends JFXApp.PrimaryStage
   { stage =>
@@ -19,12 +22,18 @@ class MainGame(val game: Game) extends JFXApp.PrimaryStage
         centerX = town.position().x_coord()
         centerY = town.position().y_coord()
         radius = town.population() / 5
-        onMouseClicked = { ae => println(s"${town.getName()} :");
-                                  println(s"   ${town.population()}");
-                                town.incrPop();
-                              radius = town.population() / 5}
+        onMouseClicked = { ae =>  town.incrPop();
+                                  radius = town.population() / 5;
+                                   {new Alert(AlertType.Information) {
+                                    initOwner(stage)
+                                    title = town.name
+                                    headerText = s"Voici quelques informations à propos de ${town.name}"
+                                    contentText = s"Population actuelle : ${town.pop}"
+                                  }.showAndWait()
+                            }}
         fill <== when(hover) choose {Yellow} otherwise Orange
         }
+
     }
 
     def roadToLine(Road : Road) : Line = {
@@ -57,7 +66,29 @@ class MainGame(val game: Game) extends JFXApp.PrimaryStage
       })
       updateTick.start()
 
-      content = edgeRoads ++ nodeTowns ++ Seq(new Button("Hell World"){layoutX <== stage.width-width; layoutY = 0},
+
+
+def newTrainWindow(): Unit = {
+    new Alert(AlertType.Information) {
+      initOwner(stage)
+      title = "Information Dialog"
+      headerText = "Look, an Information Dialog."
+      contentText = "I have a great message for you!"
+    }.showAndWait()
+}
+
+def displayTownInfo(): Unit = {
+    new Alert(AlertType.Information) {
+      initOwner(stage)
+      title = "Information Dialog"
+      headerText = "Look, an Information Dialog."
+      contentText = "I have a great message for you!"
+    }.showAndWait()
+}
+
+      content = edgeRoads ++ nodeTowns ++ Seq(new Button("New Train"){
+        onAction = handle {newTrainWindow()};
+        layoutX <== stage.width-width; layoutY = 0},
           new Button("Au revoir"){onAction = { ae => stage.close() };layoutX <== stage.width-width; layoutY = 25},
           new Button("Monde de merde"){layoutX <== stage.width-width; layoutY = 50},
           new Label(s"${game.towns()(0).population()}"){layoutX <== stage.width-width -25; layoutY <== stage.height - 75})
