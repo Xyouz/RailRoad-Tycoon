81432f853fdbb473bec6431cf29228f41be4fea2
Xyouz <marius.dufraisse@gmail.com>
2018-03-09 22:48:32 +0100

    Model.scala formatté

--- src/main/scala/model/Model.scala
+++ src/main/scala/model/Model.scala
@@ -18,7 +18,7 @@ class Point(var x : Double,var y : Double){
   def distance(p: Point): Double = {
     sqrt(pow(x-p.x_coord(),2) + pow(y-p.y_coord(),2))
   }
-  def normalize()={
+  def normalize()= {
     if (norm() != 0) { this.scale(1/norm()) }
     this
   }
@@ -27,56 +27,49 @@ class Point(var x : Double,var y : Double){
 
 // A class to represent road between two Towns
 class Road(val begin : Town,val end : Town){
-    val townsVec = end.position - begin.position
-    val length = townsVec.norm()
-    var trainsAB = Seq[Train]()
-    var trainsBA = Seq[Train]()
-    def getStart() = {begin}
-    def getEnd() = {end}
-    def numberOfTrains() = {trainsAB.length + trainsBA.length}
-    def _posTrain(t : Train, b : Boolean) =
-      {
-        var distToBegin = t.distanceOnRoad
-        if (!b) {distToBegin = length - distToBegin}
-        begin.position + townsVec.scale(distToBegin/length)
-      }
-    def getTrainsPos() = {(trainsAB.map(_posTrain(_,true)))++(trainsBA.map(_posTrain(_,false)))}
-    def update() =
-      {
-      var arrived = Seq[(Train,Int)]()
-      for (train <- trainsAB)
-        {
-        var dist = train.update()
-        if (dist >= length)
-          {
-          arrived = arrived :+ ((train, end.getID()))
-          trainsAB = trainsAB.filter(_ != train)
-          }
-        }
-      for (train <- trainsBA)
-      {
-        var dist = train.update()
-        if (dist >= length)
-          {
-            arrived = arrived :+ ((train, begin.getID()))
-            trainsBA = trainsBA.filter(_ != train)
-          }
-      }
-      arrived
-      }
-
-    def launchTrain(train : Train, destination : Town) =
-    {
-    if (destination == end)
-      {
-        trainsAB = trainsAB :+ train
-      }
-    else
-      {
-        trainsBA = trainsBA :+ train
+  val townsVec = end.position - begin.position
+  val length = townsVec.norm()
+  var trainsAB = Seq[Train]()
+  var trainsBA = Seq[Train]()
+  def getStart() = {begin}
+  def getEnd() = {end}
+  def numberOfTrains() = {trainsAB.length + trainsBA.length}
+  def _posTrain(t : Train, b : Boolean) = {
+    var distToBegin = t.distanceOnRoad
+    if (!b) {distToBegin = length - distToBegin}
+    begin.position + townsVec.scale(distToBegin/length)
+  }
+  def getTrainsPos() = {
+    (trainsAB.map(_posTrain(_,true)))++(trainsBA.map(_posTrain(_,false)))
+  }
+  def update() = {
+    var arrived = Seq[(Train,Int)]()
+    for (train <- trainsAB){
+      var dist = train.update()
+      if (dist >= length){
+        arrived = arrived :+ ((train, end.getID()))
+        trainsAB = trainsAB.filter(_ != train)
       }
     }
+    for (train <- trainsBA){
+      var dist = train.update()
+      if (dist >= length){
+        arrived = arrived :+ ((train, begin.getID()))
+        trainsBA = trainsBA.filter(_ != train)
+      }
+    }
+  arrived
   }
+
+  def launchTrain(train : Train, destination : Town) ={
+    if (destination == end){
+      trainsAB = trainsAB :+ train
+      }
+    else{
+      trainsBA = trainsBA :+ train
+      }
+  }
+}
 
 //a class to specify the goods that are available : it returns their name and their price.
 class Goods (val namegoods : String, val pricetag : Double ){}
@@ -92,45 +85,45 @@ class Town(val id : Int,
   // val coming_roads : List[(Town)],
   var pos : Point)
   {
-    override def toString() = {name}
-    var railwayStation = List[Train]()
-    def getID() : Int = {id}
-    def getName() : String = {name}
-    def position() : Point={pos}
-    def population() : Int={pop}
-    def deltaPopulation(delta : Int) = {pop += delta}
-    def incrPop() = {pop = pop+50}
-    def update()
-      {
-    //  pop += 20
-      }
-    def welcomeTrain(train : Train) = {railwayStation = train :: railwayStation
-                                        pop += train.loading;
-                                        train.loading = 0}
-    def hasTrains() : Boolean = {railwayStation.isEmpty}
-    def goodbyeTrain(train : Train) : Boolean =
-      {
-        val n = railwayStation.length
-        railwayStation = railwayStation.filter(_!=train)
-        (n != railwayStation.length)
-      }
-    }
+  override def toString() = {name}
+  var railwayStation = List[Train]()
+  def getID() : Int = {id}
+  def getName() : String = {name}
+  def position() : Point={pos}
+  def population() : Int={pop}
+  def deltaPopulation(delta : Int) = {pop += delta}
+  def incrPop() = {pop = pop+50}
+  def update(){
+  //  pop += 20
+  }
+  def welcomeTrain(train : Train) = {
+    railwayStation = train :: railwayStation
+    pop += train.loading;
+    train.loading = 0
+  }
+  def hasTrains() : Boolean = {railwayStation.isEmpty}
+  def goodbyeTrain(train : Train) : Boolean = {
+    val n = railwayStation.length
+    railwayStation = railwayStation.filter(_!=train)
+    (n != railwayStation.length)
+  }
+}
 
 
 
 //a class to represent the train, for a train, we need to know its speed, its destination, and we need a way to update its information
 class Train(val speed : Double, val name : String){
-    override def toString() = {name}
-    var distanceOnRoad : Double = -1
-    var destination = -1 // L'ID de la destination
-    def update() = { {distanceOnRoad += speed};
-                      distanceOnRoad}
-    def resetDistance() = {distanceOnRoad = 0}
-    def getDestination() = {destination}
-    def setDestination(town : Town) = {destination = town.getID()}
-    def getName() = {name}
-    var loading =  0 // number of passengers in the train.
-    def setLoading( l : Int ) = { loading = l}
+  override def toString() = {name}
+  var distanceOnRoad : Double = -1
+  var destination = -1 // L'ID de la destination
+  def update() = { {distanceOnRoad += speed};
+                    distanceOnRoad}
+  def resetDistance() = {distanceOnRoad = 0}
+  def getDestination() = {destination}
+  def setDestination(town : Town) = {destination = town.getID()}
+  def getName() = {name}
+  var loading =  0 // number of passengers in the train.
+  def setLoading( l : Int ) = { loading = l}
 }
 
 
@@ -168,24 +161,22 @@ class Game()
     new Road(townList(7),townList(1)),
     new Road(townList(9),townList(3)),
     new Road(townList(1),townList(4)),
-    new Road(townList(2),townList(10))
-    )
+    new Road(townList(2),townList(10)))
+
   var nbOfTown = townList.length
 
   var money = 0.0
   def deltaMoney(delta : Double) = {money = money + delta}
 
   //we need to define a function to find the shortest path between two towns, not necessarilly assuming that the graph of the towns is connex. We chose the algorithm of Floyd-Warshall.
-  def shortestPath(towns : Seq[Town], roads : Seq[Road]) : Array[Array[(Road,Town,Double)]] =
-  {
+  def shortestPath(towns : Seq[Town], roads : Seq[Road]) : Array[Array[(Road,Town,Double)]] = {
     val nt = towns.length
     val nr = roads.length
     val inf = Int.MaxValue
     val matrix = Array.fill[(Int, Int, Double)](nt,nt)((-2,-2,inf))
 
      // a function that maps the first two integers of the tuples with the road and the town that respectively correspond to those numbers.
-    def maps(mat1 : Array[ Array[(Int, Int, Double)]]) : Array[Array[(Road, Town, Double)]] =
-    {
+    def maps(mat1 : Array[ Array[(Int, Int, Double)]]) : Array[Array[(Road, Town, Double)]] = {
       val mat2 = Array.ofDim[(Road,Town, Double)](nt,nt)
       for (i <- 0 until nt) {
         for (j <- 0 until nt) {
@@ -195,10 +186,11 @@ class Game()
       mat2
     }
 
-    for (i <- 0 until nt) {matrix(i)(i) = (0,i,0)}
+    for (i <- 0 until nt) {
+      matrix(i)(i) = (0,i,0)
+    }
 
-    for (i <- 0 until nr)
-    {
+    for (i <- 0 until nr) {
       var j = roads(i).getEnd.getID()
       var k = roads(i).getStart.getID()
       var l = roads(i).length
@@ -228,36 +220,29 @@ class Game()
    // List of trains and the ID of the town they are currently in
   var trainsOnTransit = List[(Train, Int)]()
 
-  def trainToBeDispatched(train : Train, localState : Int) =
-  {
+  def trainToBeDispatched(train : Train, localState : Int) = {
     trainsOnTransit = (train, localState) :: trainsOnTransit
   }
 
   // townID : current town where the train is.
-  def dispatchTrain(train : Train, townID : Int) =
-  {
-    if (train.getDestination()== townID)
-      {
-        townList(townID).welcomeTrain(train)
+  def dispatchTrain(train : Train, townID : Int) = {
+    if (train.getDestination()== townID) {
+      townList(townID).welcomeTrain(train)
       }
-    else
-      {
-        train.resetDistance();
-        var info = dispatchMatrix(townID)(train.getDestination())
-        println(s"${train.toString()} redispatché vers ${info._2}")
-        (info._1).launchTrain(train,info._2)
-      }
+    else {
+      train.resetDistance();
+      var info = dispatchMatrix(townID)(train.getDestination())
+      println(s"${train.toString()} redispatché vers ${info._2}")
+      (info._1).launchTrain(train,info._2)
+    }
   }
 
-  def update() =
-  {
-    def mappingFun(t : (Train, Int)) =
-      {
-        dispatchTrain(t._1, t._2)
-      }
+  def update() = {
+    def mappingFun(t : (Train, Int)) = {
+      dispatchTrain(t._1, t._2)
+    }
 
-    for (road <- roadList)
-    {
+    for (road <- roadList) {
       trainsOnTransit = trainsOnTransit ++ road.update()
     }
     trainsOnTransit.map(mappingFun(_))
