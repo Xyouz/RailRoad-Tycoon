7a9c60d948fdd5306d9e5aebf02013ca4ddd1939
Xyouz <marius.dufraisse@gmail.com>
2018-03-11 18:56:24 +0100

    Correction des engine

--- src/main/scala/gui/planeEngine.scala
+++ src/main/scala/gui/planeEngine.scala
@@ -2,7 +2,8 @@ package planeEngine
 
 import engine._
 
-class PlaneEngine(name, maxSpeed, maxLoad) extends Engine(name, maxSpeed, maxLoad){
+class PlaneEngine( name : String, maxSpeed : Double, maxLoad : Double )
+    extends Engine(name, maxSpeed, maxLoad){
   val minSpeed = 10  // the minimal speed at which a plane can fly
   def getSpeed(load) = {
     var s = maxSpeed * (max(0,maxLoad - load))/maxLoad
--- src/main/scala/model/trainEngine.scala
+++ src/main/scala/model/trainEngine.scala
@@ -2,7 +2,8 @@ package trainEngine
 
 import engine._
 
-class TrainEngine(name, maxSpeed, maxLoad) extends Engine(name, maxSpeed, maxLoad){
+class TrainEngine(name : String, maxSpeed : Double, maxLoad : Double )
+    extends Engine(name, maxSpeed, maxLoad){
   def getSpeed(load) = {
     maxSpeed * (max(0,maxLoad - load))/maxLoad
   }
