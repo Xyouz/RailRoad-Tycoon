e6972cbc8bfbadd0c985fbae0605de10c62b521a
Xyouz <marius.dufraisse@gmail.com>
2018-03-11 18:20:20 +0100

    Affichage des véhicule généralisé

--- src/main/scala/gui/dotTrain.scala
+++ src/main/scala/gui/dotTrain.scala
@@ -2,22 +2,11 @@ package dotTrain
 
 import updatable._
 import scalafx.Includes._
-import scalafx.application.{JFXApp}
 import scalafx.scene.shape.{Circle}
 import scalafx.scene.paint.Color._
-import sendTrainDialog._
-import model._
-import town._
 import train._
+import dotVehicle.CircVehicle
 
-class CircTrain(val train : Train) extends Circle with Updatable{
-  radius = 10
+class CircTrain(train : Train) extends CircVehicle(train) with Updatable{
   fill = DarkCyan
-  update()
-
-  override def update() = {
-    var p = train.position
-    centerX = p.x_coord()
-    centerY = p.y_coord()
-  }
 }
---
+++ src/main/scala/gui/dotVehicle.scala
@@ -0,0 +1,18 @@
+package dotVehicle
+
+import updatable._
+import scalafx.Includes._
+import scalafx.scene.shape.{Circle}
+import scalafx.scene.paint.Color._
+import vehicle.Vehicle
+
+class CircVehicle(val vehicle : Vehicle) extends Circle with Updatable{
+  radius = 10
+  update()
+
+  override def update() = {
+    var p = vehicle.position
+    centerX = p.x_coord()
+    centerY = p.y_coord()
+  }
+}
