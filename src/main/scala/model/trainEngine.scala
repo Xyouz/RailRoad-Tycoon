ecb4a15f249c527f0f49a6b2a113863ba56f66cf
Xyouz <marius.dufraisse@gmail.com>
2018-03-11 19:02:17 +0100

    Correctif engine

--- src/main/scala/model/trainEngine.scala
+++ src/main/scala/model/trainEngine.scala
@@ -4,11 +4,7 @@ import engine._
 
 class TrainEngine(name : String, maxSpeed : Double, maxLoad : Double )
     extends Engine(name, maxSpeed, maxLoad){
-<<<<<<< HEAD
   def getSpeed(load : Double) = {
-=======
-  def getSpeed(load) = {
->>>>>>> 7a9c60d948fdd5306d9e5aebf02013ca4ddd1939
     maxSpeed * (max(0,maxLoad - load))/maxLoad
   }
 }
