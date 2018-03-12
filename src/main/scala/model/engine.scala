f63796a3176ddc53837c4dd25feb962f4062a905
Maaaanonnnn <manonblanc@free.fr>
2018-03-12 18:35:57 +0100

    ajout d'un prix

--- src/main/scala/model/engine.scala
+++ src/main/scala/model/engine.scala
@@ -1,6 +1,6 @@
 package engine
 
-abstract class Engine(val name : String, val maxSpeed : Double, val maxLoad : Double ){
+abstract class Engine(val name : String, val maxSpeed : Double, val maxLoad : Double, var price : Int ){
   override def toString() = {name}
   def getSpeed(load : Double) : Double
 }
