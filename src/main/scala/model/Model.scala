ef4891893104a68fb91af65a7f9461b1c01d0838
Maaaanonnnn <manonblanc@free.fr>
2018-03-23 16:05:27 +0100

    git pull

--- src/main/scala/model/Model.scala
+++ src/main/scala/model/Model.scala
@@ -76,9 +76,11 @@ class Game()
       townList(townID).loadTrain(train)
       train.nextDestination()
       }
-    train.resetDistance();
-    var info = railmap.dispatchMatrix(townID)(train.getDestination())
-    (info._1).launchTrain(train,info._2)
+    else {
+      train.resetDistance();
+      var info = railmap.dispatchMatrix(townID)(train.getDestination())
+      (info._1).launchTrain(train,info._2)
+    }
   }
 
   def update() = {
--- src/main/scala/model/railMap.scala
+++ src/main/scala/model/railMap.scala
@@ -13,7 +13,9 @@ class RailMap(val allTowns : Seq[Town], val allRoads : Seq[Road])
     val nt = towns.length
     val nr = roads.length
     val inf = Double.PositiveInfinity
-    val matrix = Array.fill[(Int, Int, Double)](nt,nt)((-1,-1,inf))
+    //val matrix = Array.fill[(Int, Int, Double)](nt,nt)((-1,-1,inf))
+    val matrixLength = Array.fill[Double](nt, nt)(inf)
+    val matrixRoads = Array.fill[Int](nt, nt)(-1)
 
    // a function that maps the first two integers of the tuples with the road and the town that respectively correspond to those numbers.
   def maps(mat1 : Array[ Array[(Int, Int, Double)]]) : Array[Array[(Road, Town, Double)]] = {
@@ -25,17 +27,31 @@ class RailMap(val allTowns : Seq[Town], val allRoads : Seq[Road])
     }
     mat2
   }
+  def maps1(matou :  Array[ Array[Int]]) : Array[Array[Road]] = {
+    val mat2 = Array.ofDim[Road](nt,nt)
+    for (i <- 0 until nt) {
+      for (j <- 0 until nt) {
+        mat2(i)(j) = (roads(max(0,mat1(i)(j)._1)))
+      }
+    }
+    mat2
+  }
 
   for (i <- 0 until nt) {
-    matrix(i)(i) = (0,i,0)
+    //matrix(i)(i) = (0,i,0)
+    matrixLength(i)(i) = 0
   }
 
   for (i <- 0 until nr) {
     var j = roads(i).getEnd.getID()
     var k = roads(i).getStart.getID()
     var l = roads(i).length
-    matrix(j)(k) = (i,k,l)
-    matrix(k)(j) = (i,j,l)
+    //matrix(j)(k) = (i,k,l)
+    //matrix(k)(j) = (i,j,l)
+    matrixLength(j)(k) = l
+    matrixLength(k)(j) = l
+    matrixRoads(j)(k) = 1
+    matrixRoads(k)(j) = 1
   }
 
 
@@ -44,16 +60,18 @@ class RailMap(val allTowns : Seq[Town], val allRoads : Seq[Road])
   for (k <- 0 until nt){
     for (i <- 0 until nt){
       for (j <- 0 until nt){
-        if (matrix(i)(j)._3 > matrix(i)(k)._3 + matrix(k)(j)._3) {
-          var d = matrix(i)(k)._3 + matrix(k)(j)._3
-          var r = matrix(i)(k)._1
-          var n = matrix(i)(k)._2
-          matrix(i)(j) = (r,n,d)
+        if (matrixLength(i)(j) > matrixLength(i)(k) + matrixLength(k)(j)) {
+          var d = matrixLength(i)(k) + matrixLength(k)(j)
+          var r = matrixRoads(i)(k)
+          //var n = matrix(i)(k)._2
+          matrixLength(i)(j) = d
+          matrixRoads(i)(j) = r
         }
       }
     }
   }
-  maps(matrix)
+  maps1(matrixRoads)
+  matrixLength
 }
 
   val dispatchMatrix = shortestPath(allTowns, allRoads)
