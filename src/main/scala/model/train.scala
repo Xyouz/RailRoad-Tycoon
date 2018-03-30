370d13b0ff1fdfe6397932155b5df8bd4913cf9c
Maaaanonnnn <manonblanc@free.fr>
2018-03-30 18:44:55 +0200

    charger/décharger ddes passagers OK

--- src/main/scala/model/train.scala
+++ src/main/scala/model/train.scala
@@ -2,9 +2,11 @@ package train
 
 import vehicle.Vehicle
 import trainEngine.TrainEngine
-
+import town._
 import wagon._
 //a class to represent trains
 class Train(name : String, engine : TrainEngine, listOfWagon : List[Wagon]) extends Vehicle(name, engine){
-
+  override def unload(t : Town) = {
+    t.pop += loading
+  }
 }
