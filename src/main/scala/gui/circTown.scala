22edfa55c5a336c0e4d8e79de24048474cca39f1
Xyouz <marius.dufraisse@gmail.com>
2018-04-02 22:59:37 +0200

    Affichage des villes avec un aéroport

--- src/main/scala/gui/circTown.scala
+++ src/main/scala/gui/circTown.scala
@@ -6,6 +6,7 @@ import scalafx.Includes._
 import scalafx.application.{JFXApp}
 import scalafx.scene.shape.{Circle}
 import scalafx.scene.paint.Color._
+import scalafx.scene.paint.{Stops, LinearGradient}
 import sendTrainDialog._
 import model._
 import town._
@@ -19,6 +20,10 @@ class CircTown(val master : JFXApp.PrimaryStage,val game : Game, val town : Town
   update()
   //onMouseClicked = handle {println(town)}
   fill = Orange
+  if (town.hasAirport){
+    fill = new LinearGradient(endX = 0,
+              stops = Stops(Orange, MidnightBlue))
+  }
 
   override def update() = {
     radius = 10//town.population() / 5
