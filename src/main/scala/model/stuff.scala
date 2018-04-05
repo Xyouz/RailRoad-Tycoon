d39b63eafa9fb4674e67dc68b58ea8c46562d8a4
Maaaanonnnn <manonblanc@free.fr>
2018-04-05 22:56:59 +0200

    stuff quasinimplémentée, par contre, exception lancée mais pas encore rattrapée si trop de stuff prélevés... (à rattraper dans les véhicules?)

--- src/main/scala/model/stuff.scala
+++ src/main/scala/model/stuff.scala
@@ -1,7 +1,28 @@
 package stuff
 
 import cargo._
+import factory._
 
-class Stuff(){
+class Stuff(val name : String, var quantity : Double){
+  def getName() : String = {name}
+  def getQuantity() : Double = {quantity}
 
+  def equalsTest(that : Stuff)  = {
+    this.name == that.name
+  }
+
+   def addQuantity(that : Stuff) = {
+    if (equals(that)) {
+      this.quantity = this.quantity + that.quantity
+    }
+  }
+
+  def subStuff(that : Stuff) = {
+    if (this.quantity >= that.quantity) {
+      this.quantity = this.quantity - that.quantity
+    }
+    else {
+      throw new Exception
+    }
+  }
 }
