4b011d40e0251a3fba4777f00c8c08a609c8bc88
Xyouz <marius.dufraisse@gmail.com>
2018-04-06 16:16:30 +0200

    Avec un écran d'acceuil

--- src/main/scala/Railroad.scala
+++ src/main/scala/Railroad.scala
@@ -4,10 +4,22 @@ import xmlParsing._
 
 import scalafx.Includes._
 import scalafx.application.JFXApp
+import scalafx.scene.Scene
+import scalafx.scene.control._
 
 
 object Program extends JFXApp {
   val loader = new XMLParser("src/main/resources/maps/debug_map.xml")
   val game = loader.getGame()
-  stage = new MainGame(game)
+
+  stage = new JFXApp.PrimaryStage {
+    title.value = "Hello Stage"
+    width = 600
+    height = 450
+    scene = new Scene {
+      content = new Button("Paf"){
+        onAction = {ae => stage = new MainGame(game)}
+      }
+    }
+  }  
 }
