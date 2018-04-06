817293596b4cca4c08f984d809c6584e85693d8f
Xyouz <marius.dufraisse@gmail.com>
2018-04-06 17:52:46 +0200

    Debut de factory data

--- src/main/scala/data/factoriesData.scala
+++ src/main/scala/data/factoriesData.scala
@@ -1,4 +1,15 @@
-package factoriesData
+package factoryBuilder
 
 import stuff._
 import stuffData._
+import factory._
+import town._
+
+class factoryBuilder(){
+  def build(typeOf : String, town : Town) = {
+    typeOf match {
+      case "aluminium_plan" => {new Factory(List())}
+      case "" =>
+    }
+  }
+}
