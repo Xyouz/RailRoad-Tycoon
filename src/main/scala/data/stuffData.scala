06e8dc6f066eff50f3b9dd43a217f6352d9fcfda
Xyouz <marius.dufraisse@gmail.com>
2018-04-06 18:57:30 +0200

    Toutes les données concernant les usines sont entrées

--- src/main/scala/data/stuffData.scala
+++ src/main/scala/data/stuffData.scala
@@ -2,51 +2,52 @@ package stuffData
 
 import stuff._
 
-class Aluminium() extends Stuff("Aluminium",0,1000){}
-class AluminumWires() extends Stuff("Aluminium Wires",0,1500){}
-class BakedGoods() extends Stuff("Baked Goods",0,1000){}
-class Bauxite() extends Stuff("Bauxite",0,1000){}
-class Beer() extends Stuff("Beer",0,1000){}
-class Bricks() extends Stuff("Bricks",0,1000){}
-class Cattle() extends Stuff("Cattle",0,1000){}
-class Cement() extends Stuff("Cement",0,1000){}
-class Chemicals() extends Stuff("Chemicals",0,1000){}
-class Clay() extends Stuff("Clay",0,1000){}
-class Coal() extends Stuff("Coal",0,1000){}
-class Copper() extends Stuff("Copper",0,1000){}
-class CopperWires() extends Stuff("CopperWires",0,1000){}
-class Cotton() extends Stuff("Cotton",0,1000){}
-class Electronics() extends Stuff("Electronics",0,1000){}
-class Fish() extends Stuff("Fish",0,1000){}
-class Fruit() extends Stuff("Fruit",0,1000){}
-class Fuel() extends Stuff("Fuel",0,1000){}
-class Furniture() extends Stuff("Furniture",0,1000){}
-class Glass() extends Stuff("Glass",0,1000){}
-class Grain() extends Stuff("Grain",0,1000){}
-class Iron() extends Stuff("Iron",0,1000){}
-class Leather() extends Stuff("Leather",0,1000){}
-class Limestone() extends Stuff("Limestone",0,1000){}
-class Liquor() extends Stuff("Liquor",0,1000){}
-class Lumber() extends Stuff("Lumber",0,1000){}
-class Marble() extends Stuff("Marble",0,1000){}
-class Meat() extends Stuff("Meat",0,1000){}
-class Milk() extends Stuff("Milk",0,1000){}
-class Oil() extends Stuff("Oil",0,1000){}
-class Paper() extends Stuff("Paper",0,1000){}
-class PetroleumProducts() extends Stuff("Petroleum Products",0,1000){}
-class Pigs() extends Stuff("Pigs",0,1000){}
-class Plastics() extends Stuff("Plastics",0,1000){}
-class Press() extends Stuff("Press",0,1000){}
-class Rubber() extends Stuff("Rubber",0,1000){}
-class Sand() extends Stuff("Sand",0,1000){}
-class Sheep() extends Stuff("Sheep",0,1000){}
-class Steel() extends Stuff("Steel",0,1000){}
-class Steel Wires() extends Stuff("Steel Wires",0,1000){}
-class Textiles() extends Stuff("Textiles",0,1000){}
-class Timber() extends Stuff("Timber",0,1000){}
-class Tyres() extends Stuff("Tyres",0,1000){}
-class Vegetables() extends Stuff("Vegetables",0,1000){}
-class Vehicles() extends Stuff("Vehicles",0,1000){}
-class Wine() extends Stuff("Wine",0,1000){}
-class Woodchips() extends Stuff("Woodchips",0,1000){}
-class Wool() extends Stuff("Wool",0,1000){}
+class Aluminium(quantity : Double) extends Stuff("Aluminium",quantity,1000){}
+class AluminumWires(quantity : Double) extends Stuff("Aluminium Wires",quantity,1500){}
+class BakedGoods(quantity : Double) extends Stuff("Baked Goods",quantity,1000){}
+class Bauxite(quantity : Double) extends Stuff("Bauxite",quantity,1000){}
+class Beer(quantity : Double) extends Stuff("Beer",quantity,1000){}
+class Bricks(quantity : Double) extends Stuff("Bricks",quantity,1000){}
+class Cattle(quantity : Double) extends Stuff("Cattle",quantity,1000){}
+class CannedFood(quantity : Double) extends Stuff("Canned Food",quantity,1000){}
+class Cement(quantity : Double) extends Stuff("Cement",quantity,1000){}
+class Chemicals(quantity : Double) extends Stuff("Chemicals",quantity,1000){}
+class Clay(quantity : Double) extends Stuff("Clay",quantity,1000){}
+class Coal(quantity : Double) extends Stuff("Coal",quantity,1000){}
+class Copper(quantity : Double) extends Stuff("Copper",quantity,1000){}
+class CopperWires(quantity : Double) extends Stuff("CopperWires",quantity,1000){}
+class Cotton(quantity : Double) extends Stuff("Cotton",quantity,1000){}
+class Electronics(quantity : Double) extends Stuff("Electronics",quantity,1000){}
+class Fish(quantity : Double) extends Stuff("Fish",quantity,1000){}
+class Fruit(quantity : Double) extends Stuff("Fruit",quantity,1000){}
+class Fuel(quantity : Double) extends Stuff("Fuel",quantity,1000){}
+class Furniture(quantity : Double) extends Stuff("Furniture",quantity,1000){}
+class Glass(quantity : Double) extends Stuff("Glass",quantity,1000){}
+class Grain(quantity : Double) extends Stuff("Grain",quantity,1000){}
+class Iron(quantity : Double) extends Stuff("Iron",quantity,1000){}
+class Leather(quantity : Double) extends Stuff("Leather",quantity,1000){}
+class Limestone(quantity : Double) extends Stuff("Limestone",quantity,1000){}
+class Liquor(quantity : Double) extends Stuff("Liquor",quantity,1000){}
+class Lumber(quantity : Double) extends Stuff("Lumber",quantity,1000){}
+class Marble(quantity : Double) extends Stuff("Marble",quantity,1000){}
+class Meat(quantity : Double) extends Stuff("Meat",quantity,1000){}
+class Milk(quantity : Double) extends Stuff("Milk",quantity,1000){}
+class Oil(quantity : Double) extends Stuff("Oil",quantity,1000){}
+class Paper(quantity : Double) extends Stuff("Paper",quantity,1000){}
+class PetroleumProducts(quantity : Double) extends Stuff("Petroleum Products",quantity,1000){}
+class Pigs(quantity : Double) extends Stuff("Pigs",quantity,1000){}
+class Plastics(quantity : Double) extends Stuff("Plastics",quantity,1000){}
+class Press(quantity : Double) extends Stuff("Press",quantity,1000){}
+class Rubber(quantity : Double) extends Stuff("Rubber",quantity,1000){}
+class Sand(quantity : Double) extends Stuff("Sand",quantity,1000){}
+class Sheep(quantity : Double) extends Stuff("Sheep",quantity,1000){}
+class Steel(quantity : Double) extends Stuff("Steel",quantity,1000){}
+class SteelWires(quantity : Double) extends Stuff("Steel Wires",quantity,1000){}
+class Textiles(quantity : Double) extends Stuff("Textiles",quantity,1000){}
+class Timber(quantity : Double) extends Stuff("Timber",quantity,1000){}
+class Tyres(quantity : Double) extends Stuff("Tyres",quantity,1000){}
+class Vegetables(quantity : Double) extends Stuff("Vegetables",quantity,1000){}
+class Vehicles(quantity : Double) extends Stuff("Vehicles",quantity,1000){}
+class Wine(quantity : Double) extends Stuff("Wine",quantity,1000){}
+class Woodchips(quantity : Double) extends Stuff("Woodchips",quantity,1000){}
+class Wool(quantity : Double) extends Stuff("Wool",quantity,1000){}
