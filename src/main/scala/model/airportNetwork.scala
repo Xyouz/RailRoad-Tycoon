2513632bab996bbd461e2003f251cdf1092585fe
Xyouz <marius.dufraisse@gmail.com>
2018-04-06 16:30:23 +0200

    Changement de l'ordre dans Dijkstra

--- src/main/scala/model/airportNetwork.scala
+++ src/main/scala/model/airportNetwork.scala
@@ -24,7 +24,9 @@ class AirportNetwork(val game : Game) {
     prec = Array.fill[Int](nt)(-1)
     visited = Array.fill(nt)(false)
     distances(startTownID) = 0
-    heap = PriorityQueue((0,startTownID))
+    heap = PriorityQueue((0.0,startTownID))(
+      implicitly[Ordering[(Double, Int)]].reverse
+    )
     briefing = List[Int](endTownID)
   }
 
