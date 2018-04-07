a303db2cda9577530b08d3c5ec536184c819e1ac
Xyouz <marius.dufraisse@gmail.com>
2018-04-07 18:12:10 +0200

    Enlève les restes de load

--- src/main/scala/model/train.scala
+++ src/main/scala/model/train.scala
@@ -4,11 +4,11 @@ import vehicle.Vehicle
 import trainEngine.TrainEngine
 import town._
 import wagon._
+
 //a class to represent trains
 class Train(name : String, engine : TrainEngine, listOfWagon : List[Wagon]) extends Vehicle(name, engine){
   def wagons() = {listOfWagon}
   override def unload(t : Town) = {
-    t.pop += loading
     val numberOfWagons = listOfWagon.length
     for (i <- 0 until numberOfWagons){
       if (listOfWagon(i).content == None) {
