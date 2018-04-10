ef991d718d2a6239023fd6aca69f0757dac9308c
Xyouz <marius.dufraisse@gmail.com>
2018-04-10 21:16:46 +0200

    Deplacement de truc

--- src/main/scala/model/cargo.scala
+++ src/main/scala/model/cargo.scala
@@ -1,6 +1,22 @@
 package cargo
 
+import stuff._
+import town._
+
 abstract class Cargo (val typeOfLoad : String, val maxLoad : Double) {
   def kindOfLoad() = {typeOfLoad}
-
+  var content : Option[Stuff] = None
+  var destination : Option[Town] = None
+  def load(toLoad : Stuff) = {
+    content = Some(toLoad)
+  }
+  def unload() = {
+    content match {
+      case None => throw new Exception
+      case Some(loading) => {
+        content = None
+        loading
+      }
+    }
+  }
 }
--- src/main/scala/model/wagon.scala
+++ src/main/scala/model/wagon.scala
@@ -6,18 +6,6 @@ import train._
 import town._
 
 class Wagon(typeOfLoad : String, maxLoad : Double) extends Cargo(typeOfLoad, maxLoad){
-  var content : Option[Stuff] = None
-  var destination : Option[Town] = None
-  def load(toLoad : Stuff) = {
-    content = Some(toLoad)
-  }
-  def unload() = {
-    content match {
-      case None => throw new Exception
-      case Some(loading) => {
-        content = None
-        loading
-      }
-    }
-  }
+
+  
 }
