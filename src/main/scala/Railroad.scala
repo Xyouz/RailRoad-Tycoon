a69595303f1d294f494c3d2100c08cf505cb0d48
Xyouz <marius.dufraisse@gmail.com>
2018-04-10 12:22:35 +0200

    Début de sélection de map

--- src/main/scala/Railroad.scala
+++ src/main/scala/Railroad.scala
@@ -6,10 +6,31 @@ import scalafx.Includes._
 import scalafx.application.JFXApp
 import scalafx.scene.Scene
 import scalafx.scene.control._
- import stuff._
+import scalafx.stage.FileChooser
+import scalafx.stage.FileChooser.ExtensionFilter
 
 object Program extends JFXApp {
-  val loader = new XMLParser("src/main/resources/maps/debug_map.xml")
-  val game = loader.getGame()
-  stage = new MainGame(game)
+  // val loader = new XMLParser("src/main/resources/maps/debug_map.xml")
+  // val game = loader.getGame()
+  // stage = new MainGame(game)
+
+  stage = new JFXApp.PrimaryStage {
+    title.value = "Hello Stage"
+    width = 600
+    height = 450
+    scene = new Scene {
+      content = Seq(
+        new Label("Roolraid Tycoan"),
+        new Button("Paf"){
+          onAction = {ae => {
+            val fileChooser = new FileChooser(){
+              title = "Choisissez une carte."
+            }
+            val mapFile = fileChooser.showOpenDialog(stage)
+          }
+        }},
+        new Button("Pif")
+      )
+    }
+  }
 }
