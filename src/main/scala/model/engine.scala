6f2c2724f20ca8e20a0f093cd7f1e5823c92948c
Maaaanonnnn <manonblanc@free.fr>
2018-04-17 14:38:51 +0200

    engine factory et mines carastérisées

--- src/main/scala/model/engine.scala
+++ src/main/scala/model/engine.scala
@@ -1,5 +1,8 @@
 package engine
 
+/** This abstract class enables us to add different kind of engines that run the planes or the trains.
+*/
+
 abstract class Engine(val name : String, val maxSpeed : Double, val maxLoad : Double, val maxRange : Double, val price : Double, val priceByKm : Double){
   override def toString() = {name}
   def getSpeed(load : Double) : Double
--- src/main/scala/model/mines.scala
+++ src/main/scala/model/mines.scala
@@ -5,6 +5,9 @@ import factory._
 import stuff._
 import town._
 
+/** A mine bahaves the same way a factory does, so it extends the class "Factory".
+*/
+
 class Mines(input : List[Stuff], output : Stuff, val ticks : Int, city : Town ) extends Factory(input, output, ticks, city) {
- 
+
 }
