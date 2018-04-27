548255f06145a5c3a7c13443574d21c1f5db2ece
Xyouz <marius.dufraisse@gmail.com>
2018-04-27 22:14:33 +0200

    Squelette pour le réseau

---
+++ src/main/scala/network/decapsulate.scala
@@ -0,0 +1,9 @@
+package decapsulate
+
+/*
+  Transform the messages receive from the network into abstract messages
+  which can be used to interact with the game
+*/
+
+import messages._
+import model._
---
+++ src/main/scala/network/encapsulate.scala
@@ -0,0 +1,7 @@
+package encapsulate
+
+import messges._
+/*
+  Transform abstract messages about game state into bytestrings
+  which can be sent through the network
+*/
