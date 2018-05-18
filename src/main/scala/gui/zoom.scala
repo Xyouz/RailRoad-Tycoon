7e577d0ceee096985998cb8d938519ed63c1f95e
Xyouz <marius.dufraisse@gmail.com>
2018-05-18 15:59:10 +0200

    zoom orthonormal

--- src/main/scala/gui/zoom.scala
+++ src/main/scala/gui/zoom.scala
@@ -17,12 +17,29 @@ import scalafx.beans.property.ReadOnlyDoubleProperty
 
 class Zoom(val stageWidth:ReadOnlyDoubleProperty, val stageHeight:ReadOnlyDoubleProperty) {
   val leftOffset = 300.0
+  var orthonormal = true
   var maxX = 1000.0
   var minX = -250.0
   var maxY= 700.0
   var minY = -300.0
 
   def transform(x : Double, y : Double) = {
+    if (orthonormal) {
+      if (maxX-minX > maxY-minY){
+        var deltaY = (maxX-minX)*(stageHeight.value)/(stageWidth.value - leftOffset)
+        var ratio = deltaY/(maxY-minY)
+        var averageY = (maxY+minY)/2
+        maxY = averageY + ratio * (maxY - averageY)
+        minY = averageY + ratio * (minY - averageY)
+      }
+      else {
+        var deltaX = (maxX-minX)*(stageWidth.value - leftOffset)/(stageHeight.value)
+        var ratio = deltaX/(maxX-minX)
+        var averageX = (maxX+minX)/2
+        maxX = averageX + ratio * (maxX - averageX)
+        minX = averageX + ratio * (minX - averageX)
+      }
+    }
     var resX = (x - minX) * (stageWidth.value - leftOffset) / (maxX - minX) + leftOffset
     var resY = (y - minY) * ( - stageHeight.value ) / (maxY - minY) + stageHeight.value
     (resX, resY)
